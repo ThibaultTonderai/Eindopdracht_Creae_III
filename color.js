3e19c6a2fb423a644b24fa7c74eb32256ca9df0c
Thibault <thibault_development@outlook.com>
2017-11-16 15:26:51 +0100

    API KEY & LINKS

--- color.js
+++ color.js
@@ -24,3 +24,12 @@ function makeSquares(colors, i){
         );
     });
 }
+
+
+// Color extraction API : Documentatie
+// https://docs.imagga.com/?php#colors
+
+// API KEY:  acc_dc8ba00c2d274a1
+// API SECRET: 7340b7dea32cc987905db48c575efd11
+
+
